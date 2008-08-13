6677c954c50cc550ab86254cfdb8d1c39c6d46c4
Alexandre Girard <alx.girard@gmail.com>
2008-08-13 18:24:04 +0200

    Add moebius example and clean some code

--- moebius/lib/prawn/font.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-require "prawn/font/wrapping"       
-require "prawn/font/metrics"
-require "prawn/font/cmap"
--- moebius/moebius.rb
+++ moebius/moebius.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/ruby
-$LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
+require "rubygems"    
+require "prawn"
 require 'fileutils'
-require 'prawn'
 
 # Check filename exists
 if ARGV[0].nil?
-  echo "Please enter a comic filename"
+  print "Please enter a comic filename\n"
   exit
 end
-
 
 basename = File.basename(ARGV[0], File.extname(ARGV[0]))
 target = "/tmp/#{basename}/"
@@ -19,6 +18,7 @@ FileUtils.mkdir_p target unless File.exists? target
 `unrar e #{ARGV[0]} #{target}` if File.extname(ARGV[0]) == "cbr"
 comicFiles = Dir.new(target).entries.sort.delete_if { |x| ! (x =~ /jpg$/) }
 
+# Generate PDF file
 Prawn::Document.generate("#{basename}.pdf",
                         :top_margin => 0, 
                         :right_margin => 0, 
@@ -26,10 +26,10 @@ Prawn::Document.generate("#{basename}.pdf",
                         :left_margin => 0,
                         :skip_page_creation => true) do
   comicFiles.each do |file|
-    #`mogrify -colorspace gray -resize 842x #{target}#{file}`
     start_new_page
     image "#{target}#{file}", :at => [0,802], :height => 842
   end
 end
 
-FileUtils.rm_r "/tmp/#{basename}/"
+# Remove tmp files
+FileUtils.rm_r target
