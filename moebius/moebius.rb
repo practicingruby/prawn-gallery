e9b082c8b5c60e28009485d939ec073edb50f1cb
Gregory <gregory.t.brown@gmail.com>
2008-08-13 13:47:20 -0400

    Tweaks to make things a little more general

--- moebius/moebius.rb
+++ moebius/moebius.rb
@@ -1,7 +1,15 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
+
+# Moebius: Simple comic book converter script.
+#
+# Things Prawn can do to make this script nicer:
+#   * Allow :margins => [0,0,0,0] or :margins => :none (or something like that)  
+
+
 require "rubygems"    
 require "prawn"
 require 'fileutils'
+require 'tempfile'
 
 # Check filename exists
 if ARGV[0].nil?
@@ -10,24 +18,25 @@ if ARGV[0].nil?
 end
 
 basename = File.basename(ARGV[0], File.extname(ARGV[0]))
-target = "/tmp/#{basename}/"
+target = "/#{Dir::tmpdir}/#{basename}/"
+                     
+# Extract comic images      
+FileUtils.mkdir_p target unless File.exists? target      
+`unzip -j #{ARGV[0]} -d #{target}` if File.extname(ARGV[0]) =~ /cbz/i
+`unrar e #{ARGV[0]} #{target}` if File.extname(ARGV[0]) =~ /cbr/i
+comicFiles = Dir["#{target}*.jpg"]      
 
-# Extract comic images
-FileUtils.mkdir_p target unless File.exists? target
-`unzip -j #{ARGV[0]} -d #{target}` if File.extname(ARGV[0]) == "cbz"
-`unrar e #{ARGV[0]} #{target}` if File.extname(ARGV[0]) == "cbr"
-comicFiles = Dir.new(target).entries.sort.delete_if { |x| ! (x =~ /jpg$/) }
-
-# Generate PDF file
-Prawn::Document.generate("#{basename}.pdf",
-                        :top_margin => 0, 
-                        :right_margin => 0, 
-                        :bottom_margin => 0, 
-                        :left_margin => 0,
-                        :skip_page_creation => true) do
+settings =  { :skip_page_creation => true,
+                   :bottom_margin => 0,        
+                      :top_margin => 0, 
+                    :right_margin => 0, 
+                     :left_margin => 0       }
+ 
+# Generate PDF file            
+Prawn::Document.generate("output/#{basename}.pdf", settings) do
   comicFiles.each do |file|
     start_new_page
-    image "#{target}#{file}", :at => [0,802], :height => 842
+    image file, :at => [0,802], :height => 842
   end
 end
 
